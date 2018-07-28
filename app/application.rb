61b97453aa0638d28b2b56fd67162958d32b64e4
Catherine Bronzo <cbronzo@mac.com>
2018-07-28 05:45:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,24 +16,36 @@ class Application
         end
       end
     if req.path.match(/add/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
+      items_to_be_added = req.params["item"]
+      if @@items.include?(items_to_be_added)
+        @@cart << items_to_be_added
+        resp.write "added #{items_to_be_added}"
+      else
+        resp.write "We don't have that item"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
-    else
-      resp.write "Path Not Found"
-    end
+      
+      
+      
+     
+     
+  #     @@items.each do |item|
+  #       resp.write "#{item}\n"
+  #     end
+  #   elsif req.path.match(/search/)
+  #     search_term = req.params["q"]
+  #     resp.write handle_search(search_term)
+  #   else
+  #     resp.write "Path Not Found"
+  #   end
 
-    resp.finish
-  end
+  #   resp.finish
+  # end
 
-  def handle_search(search_term)
-    if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
+  # def handle_search(search_term)
+  #   if @@items.include?(search_term)
+  #     return "#{search_term} is one of our items"
+  #   else
+  #     return "Couldn't find #{search_term}"
+  #   end
+  # end
 end
