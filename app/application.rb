524d5f2c76fdec8a880cc57c372f1f664510bb54
Catherine Bronzo <cbronzo@mac.com>
2018-07-28 05:41:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,12 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
+    if req.path.match(/cart/)
+      if @@cart.size == 0
+        resp.write "Your cart is empty"
+
+    
+    
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
