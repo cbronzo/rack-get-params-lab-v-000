59da74d966346e5a9ff14756a35961b766cfba6e
Catherine Bronzo <cbronzo@mac.com>
2018-07-28 05:41:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+    
+    
+    
+    
 
     if req.path.match(/items/)
       @@items.each do |item|
